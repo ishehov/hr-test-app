c20f2823e0cc8dd46407f7d0580e87e0a888b8d6
Shekhovtsov <ishehov@gmail.com>
2019-01-26 19:00:47 +0200

    Add card moving logic

--- src/components/Column.js
+++ src/components/Column.js
@@ -3,25 +3,32 @@ import Card from './Card'
 import { COLUMNS } from '../constants'
 
 class Column extends Component {
-    handleClick = id => {
+    handleClick = (id, direction) => {
         const { handleClick } = this.props;
-        handleClick(id, COLUMNS[0], COLUMNS[1]);
+        handleClick(
+            id,
+            COLUMNS[this.props.columnNumber],
+            COLUMNS[('left' === direction) ? this.props.columnNumber - 1 : this.props.columnNumber + 1]
+        );
     }
 
-    render() {
-        console.log(this.props)
 
+
+    render() {
         return(
             <div>
                 <h2>{this.props.name}</h2>
-                {/* <div>{(() => console.log(this.props.people))}</div> */}
                 {this.props.columnArray.map(id => (
                     <div key={id}>
                         <Card id={id} />
 
-                        <button onClick={() => this.handleClick(id)}>Move to Left</button>
+                            {(this.props.columnNumber !== 0) && (
+                                <button onClick={() => this.handleClick(id, 'left')}>Move to Left</button>
+                            )}
 
-                        <button onClick={() => this.handleClick(id)}>Move to Right</button>
+                            {(this.props.columnNumber !== COLUMNS.length - 1) && (
+                                <button onClick={() => this.handleClick(id, 'right')}>Move to Right</button>
+                            )}
                     </div>
                 ))}
             </div>
@@ -30,17 +37,3 @@ class Column extends Component {
 }
 
 export default Column
-
-// {{this.props.people.map(el => (
-//                         <div key={el.id.value}>
-
-//                             <p>{el.id.value}</p>
-//                             <button onClick={() => this.handleClick(el)}>click me</button>
-
-//                             {/*<p>{Object.keys(el.name).map(key => el.name[key]).join(' ')}</p>
-//                             <p>{`${el.name.title} ${el.name.first} ${el.name.last}`}</p>
-//                             <p>{el.email}</p>
-//                             <p>{el.cell}</p>
-
-//                         </div>
-//                     ))}}
