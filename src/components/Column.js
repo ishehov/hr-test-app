b60a122c7f4897030b54f3ffd2e862d4c1611062
Shekhovtsov <ishehov@gmail.com>
2019-01-29 10:43:36 +0200

    Implement redux

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,40 +1,22 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import styled from 'styled-components';
 import Card from './Card';
-import { COLUMNS } from '../constants';
 
 const ColumnName = styled.h2`
     text-align: center;
 `;
 
-class Column extends Component {
-    handleClick = (id, direction) => {
-        const { handleClick, columnNumber } = this.props;
-
-        handleClick(
-            id,
-            COLUMNS[columnNumber],
-            COLUMNS[(direction === 'left') ? columnNumber - 1 : columnNumber + 1],
-        );
-    }
-
-
-    render() {
-        return (
-            <Fragment>
-                <ColumnName>{this.props.name}</ColumnName>
-                {this.props.columnArray.map(id => (
-                    <Card
-                        id={id}
-                        columnNumber={this.props.columnNumber}
-                        handleClick={this.handleClick}
-                        key={id}
-                        people={this.props.people}
-                    />
-                ))}
-            </Fragment>
-        );
-    }
-}
+const Column = props => (
+    <Fragment>
+        <ColumnName>{props.name}</ColumnName>
+        {props.columnArray.map(id => (
+            <Card
+                id={id}
+                columnNumber={props.columnNumber}
+                key={id}
+            />
+        ))}
+    </Fragment>
+);
 
 export default Column;
--- src/reducers/dashboard.js
+++ src/reducers/dashboard.js
@@ -1,29 +1,66 @@
 
-import { COLUMNS } from '../constants';
+import {
+    COLUMNS,
+    FETCH_PEOPLE_REQUEST,
+    FETCH_PEOPLE_DONE,
+    FETCH_PEOPLE_ERROR,
+    MOVE_ID,
+    CHANGE_FILTER,
+} from '../constants';
 
 const defaultState = {
     loading: false,
-    people: [],
+    fetchError: null,
+    people: {
+        // id: personObj
+    },
     filterValue: '',
-    ...Object.assign({}, ...COLUMNS.map(el => ({ [el]: [] }))),
+    columns: {
+        // This gives us a possibility to define in constants as much columns as we want
+        ...Object.assign({}, ...COLUMNS.map(el => ({ [el]: [] }))),
+    },
 };
 
-export default (state = defaultState, action, ...rest) => {
+export default (state = defaultState, action) => {
     switch (action.type) {
-        case 'FETCH_PEOPLE.LOADING':
-            console.log(state, action, rest);
+        case FETCH_PEOPLE_REQUEST:
+            return { ...state, loading: true, fetchError: null };
+        case FETCH_PEOPLE_DONE:
+            return {
+                ...state,
+                loading: false,
+                // Building map of persons by id { id: personObj };
+                people: action.people.reduce((acc, curr) => {
+                    acc[curr.id.value] = curr;
+                    return acc;
+                }, {}),
+                // fill first column with people id's
+                columns: {
+                    ...state.columns,
+                    [COLUMNS[0]]: action.people.map(person => person.id.value),
+                },
+            };
+        case FETCH_PEOPLE_ERROR:
+            return { ...state, fetchError: action.err };
+        case MOVE_ID: {
+            const { from, to, id } = action;
+
+            if (state.columns[from].includes(id)) {
+                return {
+                    ...state,
+                    columns: {
+                        ...state.columns,
+                        [from]: state.columns[from].filter(el => (id !== el)),
+                        [to]: state.columns[to].concat(id),
+                    },
+                };
+            }
+
             return state;
-        case 'FETCH_PEOPLE.DONE':
-            console.log(state, action, rest);
-            return state;
-        case 'MOVE_ID':
-            // console.log(state, action, rest);
-            return state;
-        case 'CHANGE_FILTER':
-            // console.log(state, action, rest);
-            return state;
+        }
+        case CHANGE_FILTER:
+            return { ...state, filterValue: action.filterValue };
         default:
-            // console.log(state, action, rest);
             return state;
     }
 };
